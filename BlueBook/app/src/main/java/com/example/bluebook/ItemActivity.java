254634de4c704d24ecd523c943745a8ca74cfca4
wiwilin <katherine.linw@gmail.com>
2020-10-20 17:42:36 +1100

    Android Code

---
+++ BlueBook/app/src/main/java/com/example/bluebook/ItemActivity.java
@@ -0,0 +1,16 @@
+package com.example.bluebook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ItemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item);
+    }
+
+
+}
